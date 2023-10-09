4db70b88bb7dff2cded445e86667acdc9ea1e8fa
afonso7ss <141338152+afonso7ss@users.noreply.github.com>
2023-10-09 19:57:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get("/", function(req,res){
 
 //rota do cadastro de produtos
 app.get("/produtos", function(req,res){
-    res.send("<h1>Lista de Produtoskkkkkkkkk!</h1>");
+    res.send("<h1>Lista de Produtos hfghfgjkkkkkk!</h1>");
 })
 
 //rota com parametro 
